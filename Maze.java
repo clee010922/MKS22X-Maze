6df14947c1dee757107ea3a5105afac607ce52cc
clee010922 <clee01@stuy.edu>
2019-03-03 14:02:47 -0500

    added toString method

--- Maze.java
+++ Maze.java
@@ -34,7 +34,7 @@ public class Maze{
       c++;
     }
     int index = 0;
-    maze = new char[lineCount][lineLength];
+    maze = new char[r][c];
     while(scanner.hasNextLine()) {
       String line = scanner.nextLine();
       for (int i = 0; i < maze[0].length; i++) {
@@ -62,6 +62,17 @@ public class Maze{
     }
     catch (InterruptedException e) {
     }
+  }
+
+  public String toString() {
+    String result = "";
+    for (int r = 0; r < maze.length; r++) {
+      for (int c = 0; c < maze[0].length; c++) {
+        result += maze[r][c];
+      }
+      result += "\n";
+    }
+    return result;
   }
 
 
@@ -137,32 +148,3 @@ public class Maze{
 
 
 }
-
-/*
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-public class Maze {
-  public static void main(String args[]) throws FileNotFoundException {
-        //instead of a try/catch, you can throw the FileNotFoundException.
-        //This is generally bad behavior
-        File text = new File("Maze1.txt");
-        // can be a path like: "/full/path/to/file.txt" or "../data/file.txt"
-        //inf stands for the input file
-        Scanner inf = new Scanner(text);
-        while(inf.hasNextLine()){
-            String line = inf.nextLine();
-            System.out.println(line);//hopefully you can do other things with the line
-        }
-        int r = 0;
-        int c = 0;
-        while(inf.hasNextLine()) {
-            String line = inf.nextLine();
-            c = line.length();
-            r++;
-        }
-        System.out.println(r);
-        System.out.println(c);
-    }
-}
-*/
